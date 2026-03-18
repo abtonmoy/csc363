072f999b1066b16f85881010edc757eaf421c33e
Colin McKinney <mckinnec@wabash.edu>
2026-03-18 17:52:39 -0400

    Added files for Studio 08A

--- pa7/examples/example-ce0.asm
+++
@@ -1,8 +0,0 @@
-MV fp, sp
-JR func_main
-HALT
-
-func_main:
-    SW fp, 0(sp)
-    MV fp, sp
-    RET
--- studio08a/example02.s
+++ studio08a/example02.s
@@ -1,12 +1,12 @@
 ; Symbol table 
+; name a type InnerType.INT location 0x20000000
 ; Function: InnerType.INT main([])
 
 ; Symbol table main
-; name n type InnerType.INT location -4
+; name b type InnerType.INT location -4
 
 .section .text
-;Current temp: 
-;IR Code: 
+
 MV fp, sp
 JR func_main
 HALT
@@ -24,28 +24,15 @@ SW t3, 0(sp)
 ADDI sp, sp, -4
 SW t4, 0(sp)
 ADDI sp, sp, -4
-SW t5, 0(sp)
-ADDI sp, sp, -4
-SW t6, 0(sp)
-ADDI sp, sp, -4
-GETI t0
-SW t0, -4(fp)
-LW t2, -4(fp)
-LI t1, 2
-DIV t3, t2, t1
-SW t3, -4(fp)
-LW t4, -4(fp)
-PUTI t4
-;Current temp: t5
-;IR Code: 
-LI t5, 0
-SW t5, 8(fp)
+LA t1, 0x20000000
+LI t0, 6
+SW t0, 0(t1)
+LI t2, 7
+SW t2, -4(fp)
+LI t3, 0
+SW t3, 8(fp)
 J func_ret_main
 func_ret_main:
-ADDI sp, sp, 4
-LW t6, 0(sp)
-ADDI sp, sp, 4
-LW t5, 0(sp)
 ADDI sp, sp, 4
 LW t4, 0(sp)
 ADDI sp, sp, 4
